def3a8aae5a2e09a7fb547fc57323f2b54d4b691
ning <stan@email.ncu.edu.cn>
2018-04-04 01:34:19 +0800

    initial commit

---
+++ src/main/java/com/ncu/TrafficInformationSystem/TrafficInformationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ncu.TrafficInformationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficInformationSystemApplication.class, args);
+	}
+}
