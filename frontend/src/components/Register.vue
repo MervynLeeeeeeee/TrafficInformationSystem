66370353b4c07aa9713566e93aa48dec44f3f22a
DirtyWine <769182173@qq.com>
2018-04-22 14:59:39 +0800

    替换UI组件为Element，修改端口为8088

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,112 +1,213 @@
 <template>
-  <div class="container">
-    <div class="row no-svg-elements mbl">
-        <form role="form" class="form-view">
-          <div class="form-group">
-            <h4>SIGN UP</h4>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="email" class="form-control flat" placeholder="* Email" v-model="user_email" required>
-            <span class="form-control-feedback fui-mail"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="email" class="form-control flat" placeholder="* User name" v-model="user_name" required>
-            <span class="form-control-feedback fui-user"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="email" class="form-control flat" placeholder="* Nick name" v-model="user_nickname" required>
-            <span class="form-control-feedback fui-android"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control flat" placeholder="* Password" v-model="user_password" required>
-            <span class="form-control-feedback fui-lock"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control flat" placeholder="* Confirm password" v-model="passwords_confirm" required>
-            <span class="form-control-feedback fui-lock"></span>
-          </div>
-          <div class="form-group">
-            <div class="row">
-              <div class="col-sm-6">
-                <label class="radio">
-                  <input type="radio" data-toggle="radio" v-model="user_gender" value="male" data-radiocheck-toggle="radio" required>
-                  male
-                </label>
-              </div>
-              <div class="col-sm-6">
-                <label class="radio">
-                  <input type="radio" data-toggle="radio" v-model="user_gender" value="female" data-radiocheck-toggle="radio">
-                  female
-                </label>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="checkbox">
-              <input class="custom-checkbox" type="checkbox" data-toggle="checkbox" v-model="protocol_agreed" checked="" required>
-              I agree with
-              <a href="#">balabala</a>
-            </label>
-          </div>
-          <div class="form-group">
-            <button class="btn btn-primary btn-wide btn-block">Sign up</button>
-          </div>
-        </form>
+  <el-container>
+    <el-main>
+      <div class="form-view">
+        <el-form  ref="register" :model="register" :rules="rule_register">
+         <!--Title-->
+          <p>SIGN UP</p>
+          <!--Email-->
+          <el-form-item prop="user_email">
+            <el-input
+              v-model="register.user_email"
+              placeholder="*Email"
+              clearable>
+              <i slot="suffix" class="iconfont icon-email"/>
+            </el-input>
+          </el-form-item>
+          <!--User name-->
+          <el-form-item prop="user_name">
+            <el-input
+              v-model="register.user_name"
+              placeholder="*User name"
+              clearable>
+              <i slot="suffix" class="iconfont icon-account"/>
+            </el-input>
+          </el-form-item>
+          <!--Nick name-->
+          <el-form-item prop="user_nickname">
+            <el-input
+              v-model="register.user_nickname"
+              placeholder="*Nickname"
+              clearable>
+              <i slot="suffix" class="iconfont icon-atm"/>
+            </el-input>
+          </el-form-item>
+          <!--Password-->
+          <el-form-item prop="user_password">
+            <el-input
+              v-model="register.user_password"
+              type="password"
+              placeholder="*Password"
+              clearable>
+              <i slot="suffix" class="iconfont icon-password"/>
+            </el-input>
+          </el-form-item>
+          <!--Confirm Password-->
+          <el-form-item prop="password_confirm">
+            <el-input
+              v-model="register.password_confirm"
+              type="password"
+              placeholder="*Confirm password"
+              clearable>
+              <i slot="suffix" class="iconfont icon-password"/>
+            </el-input>
+          </el-form-item>
+          <!--Gender-->
+          <el-form-item>
+            <el-radio-group class="item-left" v-model="register.user_gender">
+              <el-radio label="male">Male</el-radio>
+              <el-radio label="female">Female</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <!--Protocol-->
+          <el-form-item>
+            <el-checkbox v-model="register.protocol_agreed" class="item-left">I agree with
+              <a href="#">some protocol</a>
+            </el-checkbox>
+          </el-form-item>
+          <!--Submit button-->
+          <el-form-item>
+            <el-button @click="submitRegister('register')" type="primary" class="btn-block">Sign up</el-button>
+          </el-form-item>
+        </el-form>
       </div>
-  </div>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
+  import axios from 'axios'
+
+  var instance  = axios.create({
+    baseURL: 'localhost:8080',
+    timeout: 1000,
+    headers: {'Test-Header': 'myHeader'}
+  })
+
   export default {
     name: "Register",
     data () {
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input password'));
+        } else {
+          if (this.register.password_confirm !== '') {
+            this.$refs['register'].validateField('password-confirm');
+          }
+          callback();
+        }
+      }
+
+      var validateCheckPass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input password again'));
+        } else if (value !== this.register.user_password) {
+          callback(new Error('Password is not consistent'));
+        } else {
+          callback();
+        }
+      }
+
       return {
-        user_email: '',
-        user_name: '',
-        user_nickname: '',
-        user_password: '',
-        passwords_confirm: '',
-        user_gender: '',
-        protocol_agreed: true
+        register: {
+          user_email: '',
+          user_name: '',
+          user_nickname: '',
+          user_password: '',
+          password_confirm: '',
+          user_gender: 'male',
+          protocol_agreed: true
+        },
+        rule_register: {
+          user_email: [
+            { required: true, message: 'Please input your email', trigger: 'blur' },
+            { type: 'email', message: 'Input must be an email address', trigger: 'blur' }
+          ],
+          user_name: [
+            { required: true, message: 'Please input username', trigger: 'blur' }
+          ],
+          user_nickname: [
+            { required: true, message: 'Please input nickname', trigger: 'blur' }
+          ],
+          user_password: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          password_confirm: [
+            { validator: validateCheckPass, trigger: 'blur' }
+          ]
+        }//rule_register
+      }
+    },
+    methods: {
+      submitRegister: function (formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            console.log('submit succeed!')
+            instance.get('/user/register', {
+              params: {
+                user: this.register
+              }
+            })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
       }
     }
   }
 </script>
 
 <style scoped>
+
   /* --- form-view --- */
   .form-view {
     background: white;
-    margin: 100px auto;
+    margin-top: 100px;
+    margin-left: auto;
+    margin-right: auto;
     padding: 20px;
     max-width: 450px;
     box-shadow: 0 0 5px white;
     transition: 0.3s;
   }
+
   .form-view:hover {
-    box-shadow: 0 0 1px #1abc9c;
+    box-shadow: 0 0 1px #409eff;
   }
-  .form-view h4,label{
+
+  .form-view p {
+    color: #3f6bcb;
+    font-size: 29px;
     text-align: left;
     font-weight: bold;
   }
+
   .form-view a {
+    text-decoration: none;
+    color: #409eff;
     font-weight: bold;
     font-size: 15px;
   }
 
-  /* --- form input style --- */
-  .form-control {
-    border-radius: 0;
-    border: none;
-    border-bottom: 3px solid white;
-    transition: 0.1s;
+  .form-view a:hover {
+    color: #234985;
   }
-  .form-control:hover {
-    border-bottom: 3px solid darkgrey;
-    mask-border-outset: 5px;
+
+  .btn-block{
+    width: 100%;
   }
-  .form-control:focus {
-    border-bottom: 3px solid #1abc9c;
+
+
+  /* --- Layout design --- */
+  .item-left{
+    float: left;
   }
+
 </style>
